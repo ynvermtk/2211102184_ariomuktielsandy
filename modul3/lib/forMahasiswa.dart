162c04c8485a2b4ba1f40fa4a1c57ab139ac94fa
Ario <ariomelsandy@gmail.com>
2025-03-09 01:09:08 +0700

    up

---
+++ modul3/lib/forMahasiswa.dart
@@ -0,0 +1,7 @@
+void main() {
+  var mahasiswa = ['Ananda', 'Surya', 'Kusuma', 'Wahyu', 'Pram'];
+
+  mahasiswa.forEach((nama) {
+    print('Nama Mahasiswa: $nama');
+  });
+}
---
+++ modul3/lib/forNIM.dart
@@ -0,0 +1,5 @@
+void main() {
+  for (int i = 21102200; i <= 21102210; i++) {
+    print('Mahasiswa ke-$i');
+  }
+}
